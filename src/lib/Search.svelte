026b7d970de72b4afc719f30766ffd8fbd238255
Samuel Amaro <samuelamaro96746313@gmail.com>
2025-02-19 22:33:40 -0300

    refactor: changes component `Search`

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import MagnifyinngGlass from './icons/MagnifyinngGlass.svelte';
+	import { replaceState } from '$app/navigation';
+	import { page } from '$app/stores';
 
 	let query: string = '';
 
@@ -19,14 +21,15 @@
 			const params = new URLSearchParams(window.location.search);
 
 			if (!value.trim()) {
-				params.delete('search');
 				query = '';
+				replaceState($page.url.pathname, {
+					search: undefined
+				});
 			} else {
-				params.set('search', value);
+				replaceState($page.url.pathname, {
+					search: value
+				});
 			}
-
-			const newUrl = params.toString() ? `?${params.toString()}` : window.location.pathname;
-			history.replaceState(null, '', newUrl);
 		}, 500);
 	}
 </script>
@@ -42,6 +45,8 @@
 			on:input={(e) => updateSearch(e.currentTarget.value)}
 			class="w-full rounded-md border border-[#D6C8E8] bg-white py-2 pl-2 pr-8 text-[#6B5A8A] focus:outline-none focus:ring-2 focus:ring-[#B084D5]"
 		/>
-		<MagnifyinngGlass className="absolute right-3 top-1/2 -translate-y-1/2 text-[#B084D5] fill-[#B084D5] w-5 h-5" />
+		<MagnifyinngGlass
+			className="absolute right-3 top-1/2 -translate-y-1/2 text-[#B084D5] fill-[#B084D5] w-5 h-5"
+		/>
 	</div>
 </form>
